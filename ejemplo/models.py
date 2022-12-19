84995b63ffb3e3f5a6a14aa2b482440b0dde1519
Maria Denise Coraglio <denisecoraglio@gmail.com>
2022-12-18 21:19:42 -0300

    Creacion de Modelos

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -10,12 +10,12 @@ class Cursos(models.Model):
 
 class Alumnos(models.Model):
     nombre = models.CharField(max_length=100)
-    direccion = models.CharField(max_length=200)
+    provincia = models.CharField(max_length=200)
     edad = models.IntegerField()
     curso = models.CharField(max_length=200)
     
     def __str__(self):
-      return f"{self.nombre}, {self.direccion}, {self.edad}, {self.curso}, {self.id}"
+      return f"{self.nombre}, {self.provincia}, {self.edad}, {self.curso}, {self.id}"
 
 class Tutores(models.Model):
     nombre = models.CharField(max_length=100)
--- seed_data.py
+++ seed_data.py
@@ -1,10 +1,17 @@
 from ejemplo.models import Cursos
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
-Cursos(nombre="Rosario", duracion="Rio Parana 745", dedicacion=123123).save()
+from ejemplo.models import Alumnos
+from ejemplo.models import Tutores
 
-print("Se cargo con éxito los cursos de pruebas")
+
+Cursos(nombre="Python", duracion="12 semanas", dedicacion="alta").save()
+Cursos(nombre="Data Engineering", duracion="42 semanas", dedicacion="alta").save()
+Cursos(nombre="Data Sciencie", duracion="42 semanas", dedicacion="alta").save()
+Cursos(nombre="Data Analytics", duracion="12 semanas", dedicacion="alta").save()
+Cursos(nombre="Procesamiento de datos en excel", duracion="4 semanas", dedicacion="moderada").save()
+Alumnos(nombre="Maria Denise Coraglio", edad=38, provincia= "Cordoba", curso="Python").save()
+Alumnos(nombre="Martin Giuliano", edad=42, provincia= "Cordoba", curso="Python").save()
+Tutores(nombre="Esteban Acevedo", curso="Python").save()
+Tutores(nombre="Enzo Martin Zotti", curso="Python").save()
+Tutores(nombre="Griselda Benitez", curso="Python").save()
+
+print("Se cargo con éxito los usuarios de pruebas")
